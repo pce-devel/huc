f1f9b8e79c0abeb4025a3c51b5f30945168a9d6a
John Brandwood <john.brandwood@telzey.com>
2022-06-06 15:35:21 -0400

    Update checksums for HuC examples after recent changes.

--- test/test_examples.sh
+++ test/test_examples.sh
@@ -18,10 +18,10 @@ echo ''
 echo Checking compiled example projects against known-good SHA1 values ...
 echo ''
 test_file ../examples/huc/acd/ac_test.iso \
- "SHA1(../examples/huc/acd/ac_test.iso)= 7a9c268dcb8d2ef982d5b7918ff66df9acddd282"
+ "SHA1(../examples/huc/acd/ac_test.iso)= 8e69b870dca009afdd5a0b7e1bc1517d08377510"
 
 test_file ../examples/huc/overlay/overlay.iso \
- "SHA1(../examples/huc/overlay/overlay.iso)= 5c5ae8951e7d48a2aba70712a159bf26fda47d4b"
+ "SHA1(../examples/huc/overlay/overlay.iso)= a91dd4e7ddc8362995f77a6d6ad6e34b81628a75"
 
 test_file ../examples/huc/promotion/promotion.pce \
  "SHA1(../examples/huc/promotion/promotion.pce)= 965bf4dcc434c2c145f3a82e33f5abeda93420dc"
@@ -30,15 +30,15 @@ test_file ../examples/huc/scroll/scroll.pce \
  "SHA1(../examples/huc/scroll/scroll.pce)= 8a4e7c910e7f4901415e06654f1f4cbfb2e52ce3"
 
 test_file ../examples/huc/sgx/sgx_test.iso \
- "SHA1(../examples/huc/sgx/sgx_test.iso)= 8be72e6d43025f207a07ea9cb18bc189026d0fc9"
+ "SHA1(../examples/huc/sgx/sgx_test.iso)= 30d45b601cf0e4bfb0c1af81801b3ca8b29454c2"
 
 test_file ../examples/huc/sgx/sgx_test.sgx \
- "SHA1(../examples/huc/sgx/sgx_test.sgx)= 49d3804cbfd252cdd98065233ac37a2d38a005c3"
+ "SHA1(../examples/huc/sgx/sgx_test.sgx)= 132833d582fa72fc36ef1b1ef295678ba1fba05f"
 
 test_file ../examples/huc/shmup/shmup.iso \
- "SHA1(../examples/huc/shmup/shmup.iso)= 8738e745b3fb7f3941f653d69c4a6d2dbb3a8e64"
+ "SHA1(../examples/huc/shmup/shmup.iso)= 6ca2cf2cb57decc7f230ba1e995ac279a332d47a"
 
 test_file ../examples/huc/shmup/shmup.pce \
- "SHA1(../examples/huc/shmup/shmup.pce)= ecea56f2dd564de06d7f1fc9f514bde0aff71990"
+ "SHA1(../examples/huc/shmup/shmup.pce)= 0871470fd364a80d4aa78076bd7dc8db1fbd1007"
 
 exit $result
