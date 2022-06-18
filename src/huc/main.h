bfd3ee00029b2335e9cfe88972b6a7e784f49fb9
John Brandwood <john.brandwood@telzey.com>
2022-06-18 14:14:40 -0400

    Bump HuC/PCEAS/ISOlink to version 4.00 after breaking changes to CD startup.

--- src/huc/main.h
+++ src/huc/main.h
@@ -15,7 +15,7 @@
 
 #include "version.h"
 
-#define HUC_VERSION     "HuC (v3.99-" GIT_VERSION ", " GIT_DATE ")"
+#define HUC_VERSION     "HuC (v4.00-" GIT_VERSION ", " GIT_DATE ")"
 
 void FEvers (void);
 void usage (char *exename);
