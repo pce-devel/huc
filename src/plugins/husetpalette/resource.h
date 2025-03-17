8c7ff0cdbbcc305c099ddcc74733979599c2002b
John Brandwood <john.brandwood@telzey.com>
2025-03-17 17:57:04 -0400

    Add a couple of plugins for Cosmigo's "Pro Motion NG" that folks will hopefully find useful. Note that these plugins only build on Windows!

---
+++ src/plugins/husetpalette/resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by resource.rc
+//
+#define ID_KEYPAD_00                    16
+#define ID_KEYPAD_01                    17
+#define ID_KEYPAD_02                    18
+#define ID_KEYPAD_03                    19
+#define ID_KEYPAD_04                    20
+#define ID_KEYPAD_05                    21
+#define ID_KEYPAD_06                    22
+#define ID_KEYPAD_07                    23
+#define ID_KEYPAD_08                    24
+#define ID_KEYPAD_09                    25
+#define ID_KEYPAD_0A                    26
+#define ID_KEYPAD_0B                    27
+#define ID_KEYPAD_0C                    28
+#define ID_KEYPAD_0D                    29
+#define ID_KEYPAD_0E                    30
+#define ID_KEYPAD_0F                    31
+#define IDD_WHICH                       101
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
