dd1da7a58ba4754cd43d633869aa927d8b0b3023
John Brandwood <john.brandwood@telzey.com>
2025-06-11 15:37:02 -0400

    Change the order of parameters in vram2vram() to conform with the older library functions.
    Clean up and add more comments to the "chrblkmap" and "multimap" examples.

--- examples/hucc/multimap/multimap.c
+++ examples/hucc/multimap/multimap.c
@@ -14,6 +14,26 @@
 ; ***************************************************************************
 ; ***************************************************************************
 ;
+; This expands upon the previous example and shows how to use a multi-screen
+; map to work around the limited 128x128 block dimensions of normal maps.
+;
+; This works by splitting a large map (up to 32768 pixels wide or high) into
+; smaller areas, each the same size as the PC Engine's BAT, which can be set
+; from 32x32 to 128x64 characters in size, with 64x32 or 32x32 as the normal
+; choices.
+;
+; Each of the smaller areas is called a "screen", and a multi-screen map can
+; use up to 1024 screens in order to build very large maps.
+;
+; While the previous example showed how to force certain 8x8 characters into
+; VRAM at specific addresses in order to perform background anbimation, this
+; expands upon the technique to show how to force specific block graphics to
+; use specific block numbers, so that the game can detect them in the map if
+; a player touches them.
+;
+; ***************************************************************************
+; ***************************************************************************
+;
 ; This uses the Seran Village map from Falcom's "Legend of Xanadu 2" game.
 ;
 ; See the README.md file in the "../seran/" directory for more information.
@@ -26,7 +46,6 @@
 #include "hucc-blkmap.h"
 
 
-
 // **************************************************************************
 // **************************************************************************
 //
@@ -34,6 +53,8 @@
 //
 // This also shows one way to animate background characters in the map.
 //
+// **************************************************************************
+// **************************************************************************
 
 
 // Get the full 256-color palette from the bitmap.
@@ -82,7 +103,7 @@ extern unsigned char seran_chr[];
 // are always 2KByte long and 2KByte aligned in memory.
 //
 // You need to tell the converter where in VRAM you are going to upload
-// the character set.
+// the character set, which is usually 0x1000 for the map graphics.
 //
 // This uses PCEAS's ability to add multiple images to a block set in order to
 // put the two blocks that make up the entrance to each house at the beginning
@@ -170,22 +191,22 @@ extern unsigned char seran_blk[];
 
 #asm
 		.data
-_multi_map:	; 1st line of 4 screens, left-to-right, each screen is 16x16=256 bytes long.
+_multi_map:	; 1st line of 4 screens, left-to-right, each screen is 16x16=$0100 bytes long.
 		SCREEN	_seran_map + $0000, _seran_blk, _seran_msk, 0
 		SCREEN	_seran_map + $0100, _seran_blk, _seran_msk, 0
 		SCREEN	_seran_map + $0200, _seran_blk, _seran_msk, 0
 		SCREEN	_seran_map + $0300, _seran_blk, _seran_msk, 0
-		; 2nd line of 4 screens, each screen is 16x16=256 bytes long.
+		; 2nd line of 4 screens, left-to-right, each screen is 16x16=$0100 bytes long.
 		SCREEN	_seran_map + $0400, _seran_blk, _seran_msk, 0
 		SCREEN	_seran_map + $0500, _seran_blk, _seran_msk, 0
 		SCREEN	_seran_map + $0600, _seran_blk, _seran_msk, 0
 		SCREEN	_seran_map + $0700, _seran_blk, _seran_msk, 0
-		; 3rd line of 4 screens, each screen is 16x16=256 bytes long.
+		; 3rd line of 4 screens, left-to-right, each screen is 16x16=$0100 bytes long.
 		SCREEN	_seran_map + $0800, _seran_blk, _seran_msk, 0
 		SCREEN	_seran_map + $0900, _seran_blk, _seran_msk, 0
 		SCREEN	_seran_map + $0A00, _seran_blk, _seran_msk, 0
 		SCREEN	_seran_map + $0B00, _seran_blk, _seran_msk, 0
-		; 4th line of 4 screens, each screen is 16x16=256 bytes long.
+		; 4th line of 4 screens, left-to-right, each screen is 16x16=$0100 bytes long.
 		SCREEN	_seran_map + $0C00, _seran_blk, _seran_msk, 0
 		SCREEN	_seran_map + $0D00, _seran_blk, _seran_msk, 0
 		SCREEN	_seran_map + $0E00, _seran_blk, _seran_msk, 0
@@ -202,6 +223,16 @@ extern unsigned char multi_map[];
 // This just gives the code an array of blank data to keep it happy.
 
 const unsigned char seran_msk[256];
+
+
+// Define the screen size that we're going to use to reduce the number of
+// unexplained "magic" numbers later on.
+
+#define VIEW_W 240
+#define VIEW_H 208
+
+#define BAT_W 256
+#define BAT_H 256
 
 
 // Define where the map's original copies of the animated characters are in VRAM
@@ -233,10 +264,11 @@ const unsigned char seran_msk[256];
 
 // Variables for the animation.
 
+unsigned char water_delay;
+unsigned char water_cycle;
 unsigned char water_frame;
 unsigned char shore_frame;
-unsigned char frame_cycle;
-unsigned int  cycle_offset;
+
 
 //
 
@@ -248,66 +280,77 @@ test_multimap()
 	set_240x208();
 
 	// Disable the display before uploading the graphics.
+
 	disp_off();
 	vsync();
 
 	// Upload the default HuCC font.
-	set_font_color(4, 0);
-	set_font_pal(15);
+
+	set_font_color( 4, 0 );
+	set_font_pal( 15 );
 	load_default_font();
 
 	// You can use COUNTOF() to get the number of palettes in a .incpal.
+
 	load_palette( 0, seran_pal, COUNTOF(seran_pal) );
 
 	// You can use SIZEOF() to get the number of bytes in a .incchr.
+
 	load_vram( 0x1000, seran_chr, SIZEOF(seran_chr) >> 1 );
 
 	// The animations need to be in VRAM to use vram2vram() DMA.
+
 	load_vram( WATER_ANIM, water_chr, SIZEOF(water_chr) >> 1 );
 	load_vram( SHORE_ANIM, shore_chr, SIZEOF(shore_chr) >> 1 );
 
-	// There are normally 256 blocks in a .incblk although there is a way
-	// to store fewer, but that isn't the point of this example.
-	set_blocks( seran_blk, seran_msk, 256 );
-
 	// We built the multi-screen map earlier, so we know that it is 4 screens wide.
+	//
+	// A multi-screen map defines the BLK used, so no call to "set_blocks()".
+
 	set_multimap( multi_map, 4 );
+
+	// The multi-screen map is drawn using global variables for the top-left
+	// coordinate in pixels, and the width and height in 8x8 characters.
 
 	vdc_map_pxl_x = 0;
 	vdc_map_pxl_y = 0;
-	vdc_map_draw_w = (240 / 8) + 1;
-	vdc_map_draw_h = (208 / 8) + 1;
+	vdc_map_draw_w = (VIEW_W / 8) + 1; // + 1 to allow for scrolling.
+	vdc_map_draw_h = (VIEW_H / 8) + 1; // + 1 to allow for scrolling.
 
 	draw_map();
 
 	put_string( "How to use a Multi-Screen map.", 0, 0 );
 	put_string( "LRUD to scroll.  RUN for next.", 0, 1 );
 
-	scroll_split(0, 0, vdc_map_pxl_x & 511, vdc_map_pxl_y & 255, BKG_ON | SPR_ON);
+	scroll_split( 0, 0, vdc_map_pxl_x & (BAT_W - 1), vdc_map_pxl_y & (BAT_H - 1), BKG_ON | SPR_ON );
 
 	vsync();
 
 	disp_on();
 
-	//  demo main loop
+	// Demo main loop.
+
 	for (;;)
 	{
 		// It is usually best to put vsync() at the top of the game loop.
+
 		vsync();
 
 		// Finish when START is pressed.
+
 		if (joytrg(0) & JOY_STRT) {
 			break;
 		}
 
 		// Get movement direction.
+
 		if (joy(0) & JOY_LEFT) {
 			if (vdc_map_pxl_x > 0)
 				vdc_map_pxl_x -= 2;
 		}
 		else
 		if (joy(0) & JOY_RIGHT) {
-			if (vdc_map_pxl_x < (1024 - 240))
+			if (vdc_map_pxl_x < (1024 - VIEW_W))
 				vdc_map_pxl_x += 2;
 		}
 
@@ -317,50 +360,45 @@ test_multimap()
 		}
 		else
 		if (joy(0) & JOY_DOWN) {
-			if (vdc_map_pxl_y < (1024 - 208))
+			if (vdc_map_pxl_y < (1024 - VIEW_H))
 				vdc_map_pxl_y += 2;
 		}
 
+		// Update the newly-visible edges of the map.
+
 		scroll_map();
 
-		scroll_split(0, 0, vdc_map_pxl_x & 511, vdc_map_pxl_y & 255, BKG_ON | SPR_ON);
+		scroll_split( 0, 0, vdc_map_pxl_x & (BAT_W - 1), vdc_map_pxl_y & (BAT_H - 1), BKG_ON | SPR_ON );
 
-		// Slow the animation by only uploading on every other video frame (each 1/60th second).
-		// Note that "irq_cnt" is a system variable that the HuCC runtime increments every video frame.
-		if (irq_cnt & 1)
-		{
-			// Upload a water frame over 4/60th, then upload a shore frame over 2/60th.
-			++frame_cycle;
-			if (frame_cycle == 6) {
-				frame_cycle = 0;
-				water_frame = ++water_frame & 7;
-				shore_frame = ++shore_frame & 3;
-			}
+		// Animate the Seran water by overwriting background characters (tiles) from
+		// copies of the animation that are already in VRAM using VRAM-to-VRAM DMA.
 
-			// Each frame of animation uploads 4 characters per cycle.
-			cycle_offset = ((frame_cycle & 3) * (4 * TILE_WORDS));
+		if (++water_delay == 6) {
+			water_delay = 0;
 
 			// Use vram2vram DMA to update the background animation in the next vblank.
-			if (frame_cycle < 4) {
-				// 8 animation frames of 4x4 characters per frame,
-				// each frame split into 4 uploads of 4 characters.
-				vram2vram( WATER_ANIM + (water_frame * WATER_SIZE) + cycle_offset, WATER_CHR + cycle_offset, (4 * TILE_WORDS) );
-			} else {
+
+			if (water_cycle ^= 1) {
 				// 4 animation frames of 8x1 characters per frame,
-				// each frame split into 2 uploads of 4 characters.
-				vram2vram( SHORE_ANIM + (shore_frame * SHORE_SIZE) + cycle_offset, SHORE_CHR + cycle_offset, (4 * TILE_WORDS) );
+				vram2vram( SHORE_CHR, SHORE_ANIM + (shore_frame * SHORE_SIZE), ( 8 * TILE_WORDS) );
+				shore_frame = ++shore_frame & 3;
+			} else {
+				// 8 animation frames of 4x4 characters per frame,
+				vram2vram( WATER_CHR, WATER_ANIM + (water_frame * WATER_SIZE), (16 * TILE_WORDS) );
+				water_frame = ++water_frame & 7;
 			}
 		}
 	}
 }
 
 
-
 // **************************************************************************
 // **************************************************************************
 //
-// Switch between two different demos ...
+// Switch between different demos (if there are different demos) ...
 //
+// **************************************************************************
+// **************************************************************************
 
 main()
 {
